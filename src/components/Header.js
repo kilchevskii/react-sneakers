5fab8aa66a8169ff8d6f9f47a5449ac3065892bc
Vlad <v.kilchevskii@ostrovchistoty.by>
2021-07-01 17:44:01 +0300

    #3

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ function Header() {
   return (
     <header className="d-flex justify-between align-center p-40">
       <div className="d-flex align-center">
-        <img width={40} height={40} src="/img/logo.png" />
+        <img width={40} height={40} src="/img/logo.png" alt="pictu" />
         <div>
           <h3 className="text-uppercase">React Sneakers</h3>
           <p className="opacity-5">Магазин лучших кроссовок</p>
@@ -10,11 +10,11 @@ function Header() {
       </div>
       <ul className="d-flex">
         <li className="mr-30">
-          <img width={18} height={18} src="/img/cart.svg" />
+          <img width={18} height={18} src="/img/cart.svg" alt="pictu" />
           <span>1205 руб.</span>
         </li>
         <li>
-          <img width={18} height={18} src="/img/user.svg" />
+          <img width={18} height={18} src="/img/user.svg" alt="pictu" />
         </li>
       </ul>
     </header>
