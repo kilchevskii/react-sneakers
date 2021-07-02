92fe55b1b2ed063ee0560b5f6a809259d05fa612
Vlad <v.kilchevskii@ostrovchistoty.by>
2021-07-02 17:29:14 +0300

    5 lesson

--- src/App.js
+++ src/App.js
@@ -1,39 +1,80 @@
-import Card from "./components/Card";
-import Header from "./components/Header";
-import Drawer from "./components/Drawer";
+import React from 'react';
+import { Route } from 'react-router-dom';
+import axios from 'axios';
+import Header from './components/Header';
+import Drawer from './components/Drawer';
 
-const arr = [
-  { title: "Мужские Кроссовки Nike Blazer Mid Suede", price: 12999, imageUrl: "/img/sneakers/1.jpg" },
-  { title: "Мужские Кроссовки Nike Air Max 270", price: 15600, imageUrl: "/img/sneakers/2.jpg" },
-  { title: "Мужские Кроссовки Nike Blazer Mid Suede", price: 8499, imageUrl: "/img/sneakers/3.jpg" },
-  { title: "Кроссовки Puma X Aka Boku Future Rider", price: 8999, imageUrl: "/img/sneakers/4.jpg" },
-];
+import Home from './pages/Home';
+import Favorites from './pages/Favorites';
 
 function App() {
+  const [items, setItems] = React.useState([]);
+  const [cartItems, setCartItems] = React.useState([]);
+  const [favorites, setFavorites] = React.useState([]);
+  const [searchValue, setSearchValue] = React.useState('');
+  const [cartOpened, setCartOpened] = React.useState(false);
+
+  React.useEffect(() => {
+    axios.get('https://60dec4a4abbdd9001722d024.mockapi.io/items').then((res) => {
+      setItems(res.data);
+    });
+    axios.get('https://60dec4a4abbdd9001722d024.mockapi.io/cart').then((res) => {
+      setCartItems(res.data);
+    });
+    axios.get('https://60dec4a4abbdd9001722d024.mockapi.io/favorites').then((res) => {
+      setFavorites(res.data);
+    });
+  }, []);
+
+  const onAddToCart = (obj) => {
+    axios.post('https://60dec4a4abbdd9001722d024.mockapi.io/cart', obj);
+    setCartItems((prev) => [...prev, obj]);
+  };
+
+  const onRemoveItem = (id) => {
+    axios.delete(`https://60dec4a4abbdd9001722d024.mockapi.io/cart/${id}`);
+    setCartItems((prev) => prev.filter((item) => item.id !== id));
+  };
+
+  const onAddToFavorite = async (obj) => {
+    try {
+      if (favorites.find((favObj) => favObj.id === obj.id)) {
+        axios.delete(`https://60dec4a4abbdd9001722d024.mockapi.io/favorites/${obj.id}`);
+      } else {
+        const { data } = await axios.post('https://60dec4a4abbdd9001722d024.mockapi.io/favorites', obj);
+        setFavorites((prev) => [...prev, data]);
+      }
+    } catch (error) {
+      alert('Не удалось добавить в фавориты');
+    }
+  };
+
+  const onChangeSearchInput = (event) => {
+    setSearchValue(event.target.value);
+  };
+
   return (
     <div className="wrapper clear">
-      <Drawer />
-      <Header />
-      <div className="content p-40">
-        <div className="d-flex align-center justify-between mb-40">
-          <h1>Все кроссовки</h1>
-          <div className="search-block d-flex">
-            <img src="/img/search.svg" alt="Search" />
-            <input placeholder="Поиск..." />
-          </div>
-        </div>
+      {cartOpened && (
+        <Drawer items={cartItems} onClose={() => setCartOpened(false)} onRemove={onRemoveItem} />
+      )}
 
-        <div className="d-flex">
-          {arr.map((obj) => (
-            <Card
-              title={obj.title}
-              price={obj.price}
-              imageUrl={obj.imageUrl}
-              onClick={() => console.log(obj)}
-            />
-          ))}
-        </div>
-      </div>
+      <Header onClickCart={() => setCartOpened(true)} />
+
+      <Route path="/" exact>
+        <Home
+          items={items}
+          searchValue={searchValue}
+          setSearchValue={setSearchValue}
+          onChangeSearchInput={onChangeSearchInput}
+          onAddToFavorite={onAddToFavorite}
+          onAddToCart={onAddToCart}
+        />
+      </Route>
+
+      <Route path="/favorites" exact>
+        <Favorites items={favorites} onAddToFavorite={onAddToFavorite} />
+      </Route>
     </div>
   );
 }
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,23 +1,38 @@
-import styles from "./Card.module.scss"
+import React from 'react';
+import styles from './Card.module.scss';
 
-// console.log(styles);
- 
-function Card(props) {
+function Card({ id, title, imageUrl, price, onFavorite, onPlus, favorited = false }) {
+  const [isAdded, setIsAdded] = React.useState(false);
+  const [isFavorite, setIsFavorite] = React.useState(favorited);
+
+  const onClickPlus = () => {
+    onPlus({ title, imageUrl, price });
+    setIsAdded(!isAdded);
+  };
+
+  const onClickFavorite = () => {
+    onFavorite({ id, title, imageUrl, price });
+    setIsFavorite(!isFavorite);
+  };
+
   return (
     <div className={styles.card}>
-      <div className={styles.favorite}>
-        <img src="/img/heart-unliked.svg" alt="Unliked" />
+      <div className={styles.favorite} onClick={onClickFavorite}>
+        <img src={isFavorite ? '/img/liked.svg' : '/img/unliked.svg'} alt="Unliked" />
       </div>
-      <img width={133} height={112} src={props.imageUrl} alt="Sneakers" />
-      <h5>{props.title}</h5>
+      <img width={133} height={112} src={imageUrl} alt="Sneakers" />
+      <h5>{title}</h5>
       <div className="d-flex justify-between align-center">
         <div className="d-flex flex-column">
           <span>Цена:</span>
-          <b>{props.price}</b>
+          <b>{price} руб.</b>
         </div>
-        <button className="button" onClick={props.onClick}>
-          <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
-        </button>
+        <img
+          className={styles.plus}
+          onClick={onClickPlus}
+          src={isAdded ? '/img/btn-checked.svg' : '/img/btn-plus.svg'}
+          alt="Plus"
+        />
       </div>
     </div>
   );
