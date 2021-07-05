894c48bf025d9f0882a4cbdbe026955b4aedf02d
Vlad <v.kilchevskii@ostrovchistoty.by>
2021-07-05 15:27:09 +0300

    url

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ function Header(props) {
 
   return (
     <header className="d-flex justify-between align-center p-40">
-      <Link to="">
+      <Link to="/">
         <div className="d-flex align-center">
           <img width={40} height={40} src="img/logo.png" alt="Logotype" />
           <div>
